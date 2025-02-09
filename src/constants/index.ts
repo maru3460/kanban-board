1f45ab650a0436703f2b35c5df7fc6531870ab72
maru <maru219142@gmail.com>
2025-02-09 17:17:03 +0900

    型定義とファイルの用意

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+export const COLUMN_NAMES = {
+  TODO: "todo",
+  IN_PROGRESS: "inProgress",
+  DONE: "done",
+} as const;
+
+export const COLUMN_TITLES = {
+  [COLUMN_NAMES.TODO]: "Todo",
+  [COLUMN_NAMES.IN_PROGRESS]: "In Progress",
+  [COLUMN_NAMES.DONE]: "Done",
+} as const;
+
+export const ItemTypes = {
+  TASK: "TASK",
+} as const;
